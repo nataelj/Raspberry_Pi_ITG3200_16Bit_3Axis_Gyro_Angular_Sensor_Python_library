85effd33af4ca5907064e1822bbd276c0821913e
nataelj <nataelj@yahoo.com>
2019-01-06 14:44:13 +0100

    Temperature added, degrees converting, and interrupt setup
    Temperature function and corresponding scaling added (as yet untested)
    Function to convert gyro data to degrees added, and original function renamed raw.
    Pins for interrupts added based on great C++ code at https://github.com/ncdcommunity/Arduino_Library_ITG3200_16Bit_3Axis_Gyro_Angular_Sensor/blob/master/ITG_3200.cpp

--- ITG3200.py
+++ ITG3200.py
@@ -2,10 +2,15 @@
 # Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
 # ITG3200
 # This code is designed to work with the ITG-3200_I2CS I2C Mini Module available from ControlEverything.com.
-# https://www.controleverything.com/content/Gyro?sku=ITG-3200_I2CS#tabs-0-product_tabset-2
+# https:#www.controleverything.com/content/Gyro?sku=ITG-3200_I2CS#tabs-0-product_tabset-2
 
 import smbus
 import time
+
+# sensitivity and offset
+TEMP_SENSITIVITY = 280 # 280 LSB per °C
+TEMP_OFFSET = (13200 + 35*TEMP_SENSITIVITY) # 13200 LSB + 35°C
+GYRO_SENSITIVITY = 14.375 # 14.375 LSB per °/s
 
 # Get I2C bus
 bus = smbus.SMBus(1)
@@ -39,6 +44,40 @@ ITG3200_DLPF_BW_20					= 0x04 # Bandwidth = 20Hz
 ITG3200_DLPF_BW_10					= 0x05 # Bandwidth = 10Hz
 ITG3200_DLPF_BW_5					= 0x06 # Bandwidth = 5Hz
 
+# ITG3200 Interrupt Config Register
+ITG_3200_REG_GYRO_INTCNF_ACTL_MASK 				= 0x80      # Logic level for INT Output Pin
+ITG_3200_REG_GYRO_INTCNF_ACTL_HIGH            	= 0x00      # Active High
+ITG_3200_REG_GYRO_INTCNF_ACTL_LOW               = 0x80      # Active Low
+
+ITG_3200_REG_GYRO_INTCNF_OPEN_MASK              = 0x40      # Drive Type for INT Output Pin
+ITG_3200_REG_GYRO_INTCNF_OPEN_PUSH_PULL         = 0x00      # Push-Pull
+ITG_3200_REG_GYRO_INTCNF_OPEN_OPEN              = 0x40      # Open Drain
+
+ITG_3200_REG_GYRO_INTCNF_LATCH_INT_EN_MASK      = 0x20      # Latch Mode
+ITG_3200_REG_GYRO_INTCNF_LATCH_INT_EN_50US      = 0x00      # 50us Pulse
+ITG_3200_REG_GYRO_INTCNF_LATCH_INT_EN_LATCH     = 0x20      # Latch until Interrupt is Cleared
+
+ITG_3200_REG_GYRO_INTCNF_INT_ANYRD_2CLEAR_MASK  = 0x10      # Latch Clear Method
+ITG_3200_REG_GYRO_INTCNF_INT_ANYRD_2CLEAR_STAT  = 0x00      # Status Register Read Only
+ITG_3200_REG_GYRO_INTCNF_INT_ANYRD_2CLEAR_ANY   = 0x10      # Any Register Read
+
+ITG_3200_REG_GYRO_INTCNF_INT_ITG_RDY_EN_MASK    = 0x04      # Interrupt Status when Device is Ready (PLL Ready after changing Clock Source)
+ITG_3200_REG_GYRO_INTCNF_INT_ITG_RDY_EN_DISABLE = 0x00      # Disable Interrupt when Device is Ready (PLL Ready after changing Clock Source)
+ITG_3200_REG_GYRO_INTCNF_INT_ITG_RDY_EN_ENABLE  = 0x04      # Enable Interrupt when Device is Ready (PLL Ready after changing Clock Source)
+
+ITG_3200_REG_GYRO_INTCNF_INT_RAW_RDY_EN_MASK    = 0x01      # Interrupt Status when Data is Ready
+ITG_3200_REG_GYRO_INTCNF_INT_RAW_RDY_EN_DISABLE = 0x00      # Disable Interrupt when Data is Ready
+ITG_3200_REG_GYRO_INTCNF_INT_RAW_RDY_EN_ENABLE  = 0x01      # Enable Interrupt when Data is Ready
+
+#INTERRUPT STATUS REGISTER
+ITG_3200_REG_GYRO_INTSTAT_INT_ITG_RDY_MASK      = 0x04      # PLL Ready Status
+ITG_3200_REG_GYRO_INTSTAT_INT_ITG_RDY_NOT       = 0x00      # PLL Not Ready
+ITG_3200_REG_GYRO_INTSTAT_INT_ITG_RDY_READY     = 0x04      # PLL Ready
+
+ITG_3200_REG_GYRO_INTSTAT_INT_RAW_RDY_MASK      = 0x01      # Raw Data Ready Status
+ITG_3200_REG_GYRO_INTSTAT_INT_RAW_RDY_NOT       = 0x00      # Raw Data Not Ready
+ITG_3200_REG_GYRO_INTSTAT_INT_RAW_RDY_READY     = 0x01      # Raw Data is Ready
+
 # ITG3200 Power Management Register
 ITG3200_PWR_H_RESET					= 0x80 # Reset device and internal registers to the power-up-default settings
 ITG3200_PWR_SLEEP					= 0x40 # Enable low power sleep mode
@@ -53,51 +92,115 @@ ITG3200_CLOCK_PLL_ZGYRO				= 0x03 # PLL with Z Gyro reference
 ITG3200_CLOCK_PLL_EXT32K			= 0x04 # PLL with external 32.768kHz reference
 ITG3200_CLOCK_PLL_EXT19M			= 0x05 # PLL with external 19.2MHz reference
 
+calibration =[ 105.10601, -16.84151, 12.15584] #Zero offsets, X,Y,Z
+
+# TODO Add interrupt managing capability - cpp examples at https:#github.com/Cameri/Itg3200 and https://github.com/ncdcommunity/Arduino_Library_ITG3200_16Bit_3Axis_Gyro_Angular_Sensor
+
 class ITG3200():
 	def __init__(self):
 		self.power_configuration()
 		self.fullscale_configuration()
+
+	def __del__(self):
+		# Set the sleep bit to on in the power management register to sleep the device
+		bus.write_byte_data(ITG3200_DEFAULT_ADDRESS,ITG3200_PWR_MGM, ITG3200_PWR_SLEEP)
 	
 	def power_configuration(self):
 		"""Select the Power Management Register configuration of the gyroscope from the given provided values"""
-		POWER_CONFIG = (ITG3200_CLOCK_PLL_XGYRO | ITG3200_PWR_NRML_X_Y_Z)
+		POWER_CONFIG = (ITG3200_CLOCK_PLL_XGYRO | ITG3200_PWR_NRML_X_Y_Z) # Note: This is a bitwise or operator to set the appropriate bits on the power management register
 		bus.write_byte_data(ITG3200_DEFAULT_ADDRESS, ITG3200_PWR_MGM, POWER_CONFIG)
+		
+
+	def verify_device_identity(self):
+		return bus.read_byte_data(ITG3200_DEFAULT_ADDRESS,ITG3200_WHO_AM_I)
 	
 	def fullscale_configuration(self):
 		"""Select the Digital Low Pass Filter Register configuration of the gyroscope from the given provided values"""
 		FULLSCALE_CONFIG = (ITG3200_FULLSCALE_2000 | ITG3200_DLPF_BW_256)
 		bus.write_byte_data(ITG3200_DEFAULT_ADDRESS, ITG3200_DLPF_PS, FULLSCALE_CONFIG)
+
+	def read_temperature(self):
+		"""Read data back from ITG3200_TEMP_OUT_H(0x1B), 2 bytes
+		Adjust by offset and scale by sensitivity and then return.
+		Temperature given in degrees C"""
+		# Read a block of 2 bytes from ITG3200_DEFAULT_ADDRESS, offset ITG3200_TEMP_OUT_H
+		data = bus.read_i2c_block_data(ITG3200_DEFAULT_ADDRESS, ITG3200_TEMP_OUT_H, 2)
+
+		# Convert the first two bytes (presumably the high and low) to an unsigned int equivalent
+		temp = data[0] * 256 + data[1]
+		if temp > 32767 : # If value is indeed an unsigned int equivalent (e.g. 0 to 65,535) in 2's complement format
+			temp -= 65536 # Convert it to signed int equivalent (-32,767 to 32,767)
+
+		temp+=TEMP_OFFSET # Add temperature offset
+		temp/=TEMP_SENSITIVITY # Scale by temperature sensitivity
+		# TODO Check this- maybe need a negative sign here? Check for inversion need
+
+		return temp
 	
-	def read_gyro(self):
+	def read_gyro_raw(self):
 		"""Read data back from ITG3200_GYRO_XOUT_H(0x1D), 6 bytes
-		X-Axis MSB, X-Axis LSB, Y-Axis MSB, Y-Axis LSB, Z-Axis MSB, Z-Axis LSB"""
+		X-Axis MSB, X-Axis LSB, Y-Axis MSB, Y-Axis LSB, Z-Axis MSB, Z-Axis LSB
+		Data returned as a dictionary with keys x, y, and z and values in units of LSB."""
+		# Read a block of 6 bytes from ITG3200_DEFAULT_ADDRESS, offset ITG3200_GYRO_XOUT_H
 		data = bus.read_i2c_block_data(ITG3200_DEFAULT_ADDRESS, ITG3200_GYRO_XOUT_H, 6)
 		
-		# Convert the data
+		# Convert the first two bytes (presumably the high and low) to an unsigned int equivalent
 		xGyro = data[0] * 256 + data[1]
-		if xGyro > 32767 :
-			xGyro -= 65536
+		if xGyro > 32767 : # If value is indeed an unsigned int equivalent (e.g. 0 to 65,535) in 2's complement format
+			xGyro -= 65536 # Convert it to signed int equivalent (-32,767 to 32,767)
 		
+		# Convert the next two bytes (presumably the high and low) to an unsigned int equivalent
 		yGyro = data[2] * 256 + data[3]
-		if yGyro > 32767 :
-			yGyro -= 65536
+		if yGyro > 32767 : # If value is an unsigned int equivalent (e.g. 0 to 65,535) in 2's complement format
+			yGyro -= 65536 # Convert it to signed int equivalent (-32,767 to 32,767)
 		
+		# Convert the next two bytes (presumably the high and low) to an unsigned int equivalent
 		zGyro = data[4] * 256 + data[5]
-		if zGyro > 32767 :
-			zGyro -= 65536
+		if zGyro > 32767 : # If value is an unsigned int equivalent (e.g. 0 to 65,535) in 2's complement format
+			zGyro -= 65536 # Convert it to signed int equivalent (-32,767 to 32,767)
+			
+		#Apply calibrations
+		xGyro = xGyro + calibration[0]
+		yGyro = yGyro + calibration[1]
+		zGyro = zGyro + calibration[2]
 		
+		# Return a new dictionary
 		return {'x' : xGyro, 'y' : yGyro, 'z' : zGyro}
 
-from ITG3200 import ITG3200
-itg3200 = ITG3200()
+	def read_gyro(self):
+		"""Read raw data from gyro and then scale it and return.
+		Data returned as a dictionary with keys x, y, and z and values in degrees/S."""
+		data=read_gyro_raw() # Read the raw values
 
-while True :
-	itg3200.power_configuration()
-	itg3200.fullscale_configuration()
-	time.sleep(0.5)
-	gyro = itg3200.read_gyro()
-	print "X-Axis of Rotation : %d" %(gyro['x'])
-	print "Y-Axis of Rotation : %d" %(gyro['y'])
-	print "Z-Axis of Rotation : %d" %(gyro['z'])
-	print " ************************************* "
-	time.sleep(0.5)
+		# Scale the values by the sensitivity
+		xGyro=data['x']/GYRO_SENSITIVITY
+		yGyro=data['y']/GYRO_SENSITIVITY
+		zGyro=data['z']/GYRO_SENSITIVITY
+
+		# Return a new dictionary
+		return {'x' : xGyro, 'y' : yGyro, 'z' : zGyro}
+
+
+if __name__ == "__main__":
+    # if run directly we'll just create an instance of the class and output the current readings
+	itg3200 = ITG3200()
+
+	print('device id',itg3200.verify_device_identity())
+
+	while True :
+		itg3200.power_configuration()
+		itg3200.fullscale_configuration()
+		time.sleep(0.5)
+		gyro = itg3200.read_gyro_raw()
+		print("Raw data readings, use to calibrate")
+		print("X-Axis of Rotation : %d" %(gyro['x']))
+		print("Y-Axis of Rotation : %d" %(gyro['y']))
+		print("Z-Axis of Rotation : %d" %(gyro['z']))
+		print(" ************************************* ")
+		gyro = itg3200.read_gyro()
+		print("Scaled data readings in degrees")
+		print("X-Axis of Rotation : %d" %(gyro['x']))
+		print("Y-Axis of Rotation : %d" %(gyro['y']))
+		print("Z-Axis of Rotation : %d" %(gyro['z']))
+		print(" ************************************* ")
+		time.sleep(0.5)
